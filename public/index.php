e6c0de3b8a88c5193133c6e2813d2cf13c02835a
felix-hauger <felix.hauger@laplateforme.io>
2023-05-16 14:59:45 +0200

    Set env variables in index.php

--- public/index.php
+++ public/index.php
@@ -4,4 +4,4 @@ use App\Config\DotEnv;
 
 require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
-$test = new DotEnv();
+DotEnv::setEnvVariables();
--- src/Config/DotEnv.php
+++ src/Config/DotEnv.php
@@ -2,11 +2,23 @@
 
 namespace App\Config;
 
-
 class DotEnv
 {
-    public function __construct()
+    /**
+     * Set environment variables with .env file
+     */
+    public static function setEnvVariables(): void
     {
-        
+        $file = file(dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . '.env', FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES);
+
+        foreach ($file as $line) {
+            if ($line[0] !== '#' && $line[0] !== ';') {
+                // Get Key & Value from line
+                $pair = explode('=', $line);
+
+                // Set environment variable with found pair
+                $_ENV[$pair[0]] = $pair[1];
+            }
+        }
     }
 }
