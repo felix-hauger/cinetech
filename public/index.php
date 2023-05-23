025a823784b71697feb1a66c8d7a3c60c2339d28
felix-hauger <felix.hauger@laplateforme.io>
2023-05-23 15:09:50 +0200

    Map route for homepage + match routes

--- public/index.php
+++ public/index.php
@@ -5,3 +5,31 @@ use App\Config\DotEnv;
 require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 DotEnv::setEnvVariables();
+
+setcookie('api_key', $_ENV['API_KEY'], time() + 3600 * 24 * 365 * 10, '/cinetech', '', false, false);
+
+$router = new AltoRouter();
+
+// Must set base path if the app is not at the website root
+$router->setBasePath('/cinetech');
+
+// Map route for homepage
+$router->map('GET', '/', 'App\\Controller\\Home#index', 'homepage');
+
+// Match current request url
+$match = $router->match();
+
+// If target is a string parse it into a class method
+if (is_array($match) && is_string($match['target'])) {
+    $match['target'] = explode('#', $match['target']);
+
+    $match['target'][0] = new $match['target'][0];
+}
+
+// Call closure or throw 404 status
+if(is_array($match) && is_callable( $match['target'])) {
+	call_user_func_array($match['target'], $match['params']);
+} else {
+	// No route was matched
+	header($_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+}
